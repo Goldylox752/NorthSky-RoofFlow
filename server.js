0f10ca2a31ce00c6ff10035e93ab5d55a3da61cf
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-05-03 10:41:52 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+require("./config/env"); // 
+
+
 require("dotenv").config();
 
 const express = require("express");
