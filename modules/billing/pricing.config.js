1740d6a9996f4e492f1eba186b0be4bb72b6acfc
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-05-16 13:18:54 -0600

    Create pricing.config.js

---
+++ modules/billing/pricing.config.js
@@ -0,0 +1,14 @@
+export const PRICES = Object.freeze({
+  starter: {
+    amount: 1000,
+    name: "Starter",
+  },
+  growth: {
+    amount: 2000,
+    name: "Growth",
+  },
+  elite: {
+    amount: 5000,
+    name: "Elite",
+  },
+});
