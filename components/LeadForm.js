c635d5875dae9a09f15a42dc8292ed743bc793db
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-05-20 13:40:30 -0600

    Create LeadForm.js

---
+++ components/LeadForm.js
@@ -0,0 +1,10 @@
+export default function LeadForm() {
+  return (
+    <form>
+      <input placeholder="Name" />
+      <input placeholder="Email" />
+      <input placeholder="Company" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
