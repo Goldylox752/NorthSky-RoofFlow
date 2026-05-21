a3323bbf5516c45287040cc146cb749c4ba62880
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-05-20 20:53:44 -0600

    Delete error.middleware.js

--- middleware/error.middleware.js
+++
@@ -1,8 +0,0 @@
-module.exports = function errorHandler(err, req, res, next) {
-  console.error("API Error:", err);
-
-  res.status(500).json({
-    success: false,
-    error: err.message || "Server error",
-  });
-};
